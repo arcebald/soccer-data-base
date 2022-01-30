aa1c04a3a2232107a90472bbe04c915008f3f7c7
simion <jardan.simion@gmail.com>
2022-01-30 16:13:08 -0600

    generated getters and setters for team class

--- src/main/java/com/soccer/model/Team.java
+++ src/main/java/com/soccer/model/Team.java
@@ -34,5 +34,72 @@ public class Team {
     @JsonIgnore
     private League league;
 
+    public Team() {
+    }
 
+    public Team(Long id, String name, Integer clubValue) {
+        this.id = id;
+        this.name = name;
+        this.clubValue = clubValue;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getClubValue() {
+        return clubValue;
+    }
+
+    public void setClubValue(Integer clubValue) {
+        this.clubValue = clubValue;
+    }
+
+    public TeamAddress getTeamAddress() {
+        return teamAddress;
+    }
+
+    public void setTeamAddress(TeamAddress teamAddress) {
+        this.teamAddress = teamAddress;
+    }
+
+    public List<Player> getPlayersList() {
+        return playersList;
+    }
+
+    public void setPlayersList(List<Player> playersList) {
+        this.playersList = playersList;
+    }
+
+    public League getLeague() {
+        return league;
+    }
+
+    public void setLeague(League league) {
+        this.league = league;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", clubValue=" + clubValue +
+                ", teamAddress=" + teamAddress +
+                ", playersList=" + playersList +
+                ", league=" + league +
+                '}';
+    }
 }
