6c3d5931a5632dd9fd0dce52a48edcdc7cf1c576
simion <jardan.simion@gmail.com>
2022-02-04 09:36:18 -0600

    added @Restcontroller to the address controller

--- src/main/java/com/soccer/controller/AddressController.java
+++ src/main/java/com/soccer/controller/AddressController.java
@@ -6,7 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping(path = "/api")
 public class AddressController {
     private AddressService addressService;
 
